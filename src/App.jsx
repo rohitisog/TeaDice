a728302541b2397b600dcd55645ef1dee86bab40
rohitisog <rohitisog@gmail.com>
2025-02-27 23:12:41 +0530

    fix coonect wallet toast warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ethers } from "ethers";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import PlayDice from "./pages/PlayDice";
 import LeaderBoard from "./pages/LeaderBoard";
 import NFTMarketplace from "./pages/NFTMarketplace";
 import UserProfile from "./pages/UserProfile";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { contractAddress, abi } from "./contract";
 
 const MONAD_TESTNET_PARAMS = {
@@ -35,17 +35,18 @@ const App = () => {
         await provider.send("eth_requestAccounts", []);
         const signer = await provider.getSigner();
         const contractInstance = new ethers.Contract(contractAddress, abi, signer);
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+
         setAccount(accounts[0]);
         setDiceGameContract(contractInstance);
         checkNetwork();
+        toast.success(`Wallet Connected: ${accounts[0].slice(0, 6)}...${accounts[0].slice(-4)}`);
       } catch (error) {
+        toast.error("Wallet connection failed!");
         console.error("WALLET CONNECTION FAILED:", error);
       }
     } else {
-      alert("PLEASE INSTALL METAMASK!");
+      toast.warning("Please install MetaMask!");
     }
   };
 
@@ -61,6 +62,7 @@ const App = () => {
             params: [{ chainId: MONAD_TESTNET_PARAMS.chainId }],
           });
           setNetwork(MONAD_TESTNET_PARAMS.chainId);
+          toast.info("Switched to Monad Testnet");
         } catch (switchError) {
           if (switchError.code === 4902) {
             try {
@@ -68,10 +70,13 @@ const App = () => {
                 method: "wallet_addEthereumChain",
                 params: [MONAD_TESTNET_PARAMS],
               });
+              toast.success("Monad Testnet Added!");
             } catch (addError) {
+              toast.error("Failed to add Monad Testnet!");
               console.error("FAILED TO ADD MONAD TESTNET:", addError);
             }
           } else {
+            toast.error("Error switching network!");
             console.error("ERROR SWITCHING NETWORK:", switchError);
           }
         }
@@ -81,6 +86,7 @@ const App = () => {
 
   // Disconnect wallet simply by clearing the account and contract state.
   const disconnectWallet = () => {
+    toast.info("Wallet Disconnected");
     setAccount(null);
     setDiceGameContract(null);
   };
@@ -97,18 +103,12 @@ const App = () => {
       <Header account={account} connectWallet={connectWallet} disconnectWallet={disconnectWallet} />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route
-          path="/play-dice"
-          element={<PlayDice diceGameContract={diceGameContract} account={account} />}
-        />
+        <Route path="/play-dice" element={<PlayDice diceGameContract={diceGameContract} account={account} />} />
         <Route path="/leaderboard" element={<LeaderBoard diceGameContract={diceGameContract} />} />
         <Route path="/nft-marketplace" element={<NFTMarketplace />} />
-        <Route
-          path="/user-profile"
-          element={<UserProfile account={account} diceGameContract={diceGameContract} />}
-        />
+        <Route path="/user-profile" element={<UserProfile account={account} diceGameContract={diceGameContract} />} />
       </Routes>
-      <ToastContainer />
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
     </Router>
   );
 };
